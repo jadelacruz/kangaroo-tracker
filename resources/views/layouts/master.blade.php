b91a3ce127183bd4aea4ddae26ee895bb3f6c462
Aldrin (Jhon) <jhon.aldrin.delacruz@gmail.com>
2023-01-18 18:43:12 +0800

    fix: added the page content to master layout page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,36 +6,10 @@
 <body class="dx-viewport">
 
     @include('partials.nav')
-    
-    <div class="container">
-        <div class="row">
 
-            <div class="col-md-12">
-                <p class="h3">Kangaroo Tracker</p>
-                <hr />
-            </div>
-
-            <div class="col-md-4 mb-3">
-                <div class="btn-group" role="group">
-                    <button type="button" class="btn btn-success">Add <i class="fa fa-plus"></i></button>
-                    <button type="button" class="btn btn-primary">Update <i class="fa fa-pencil"></i></button>
-                    <button type="button" class="btn btn-warning">Delete <i class="fa fa-trash"></i></button>
-                </div>
-            </div>
-
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="row">
-
-            <div class="col-md-12">
-                <div id="gridContainer"></div>
-            </div>
-
-        </div>
-    </div>
+    @yield('content')
 
     @include('partials.script')
+
 </body>
 </html>
